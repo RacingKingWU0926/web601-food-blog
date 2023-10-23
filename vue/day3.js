8fd8614f394b86cef090ab10fc98269746d06a3b
JasonLei <jlei@activision.com>
2023-10-23 10:30:02 -0700

    completed day3:

--- vue/day3.js
+++ vue/day3.js
@@ -76,6 +76,38 @@ new Vue({
                 return '';
             }
         },
+    },
 
-    }
+    components: {
+        'app-nav': {
+            template: `
+                <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+                    <span>Food Blog</span>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Recipes</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Lifestyles</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Videos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            `,
+        },
+    },
+
 });
